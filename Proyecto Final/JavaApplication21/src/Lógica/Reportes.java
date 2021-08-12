af724df158da600d9d2c264764cd68c1a7646acc
Chuby2425 <jeisonaguilar2783956@gmail.com>
2021-08-11 20:48:21 -0600

    preuba
    listo

--- Proyecto Final/JavaApplication21/src/Lógica/Reportes.java
+++ Proyecto Final/JavaApplication21/src/Lógica/Reportes.java
@@ -23,7 +23,9 @@ public class Reportes {
     FileOutputStream archivo;
     Paragraph titulo,
             ubicacion,
-            codigoPostal;
+            codigoPostal,
+            paginaWeb
+            ;
 
     public Reportes(String nombreEmpresa, String Fecha, ArrayList<DatosReporte> productos, String rutaImagen) {
         this.nombreEmpresa = nombreEmpresa;
@@ -35,6 +37,8 @@ public class Reportes {
         titulo = new Paragraph("REPORTE DE VENTAS ");
         ubicacion = new Paragraph("Ubicacion: San José,Puriscal,Santiago");
         codigoPostal = new Paragraph("Codigo Postal: 10401");
+        paginaWeb = new Paragraph("La pagina web esta funcionando androides ");
+        
 
     }
 
@@ -57,7 +61,7 @@ public class Reportes {
             ubicacion.setAlignment(Element.ALIGN_LEFT);
             documento.add(Chunk.NEWLINE);
             codigoPostal.setAlignment(Element.ALIGN_LEFT);
-
+            paginaWeb.setAlignment(Element.ALIGN_LEFT);
             // para agregar imagenes y darle posicion 
             Image imagen = null;
 
@@ -73,6 +77,7 @@ public class Reportes {
             documento.add(titulo);
             documento.add(ubicacion);
             documento.add(codigoPostal);
+            documento.add(paginaWeb);
             documento.close();
             JOptionPane.showMessageDialog(null, "El reporte se a generado -_-");
 
